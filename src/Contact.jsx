1827a6b3dd29a14201d0a1fc995968b6f507dd85
Christian <christianugoo@gmail.com>
2022-10-18 02:37:02 +0100

    updates

---
+++ src/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Contact({numbers}) {
+
+  return (
+    <div>
+      {numbers.map((n, i) => (
+        <p key={i}>{n}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Contact
